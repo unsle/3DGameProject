46d2a6502d7dcae35da414afcda3c19bf32d584d
unsle <nayyeon.dgu@gmail.com>
2021-06-12 18:37:05 +0900

    NowOK

--- SF/Assets/Scripts/WeaponController.cs
+++ SF/Assets/Scripts/WeaponController.cs
@@ -6,27 +6,15 @@ public class WeaponController : MonoBehaviour
 {
     public GameObject mage;
     public GameObject knight;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void Mage()
     {
-        Debug.Log("Mage");
         mage.SetActive(true);
         knight.SetActive(false);
     }
 
     public void Knight()
     {
-        Debug.Log("Knight");
         mage.SetActive(false);
         knight.SetActive(true);
     }
