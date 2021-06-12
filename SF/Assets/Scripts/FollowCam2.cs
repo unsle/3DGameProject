584d1651450b7a4936b141e2814e2b17c209d920
mais0j0aime <40887199+mais0j0aime@users.noreply.github.com>
2021-06-12 17:02:22 +0900

    0612
    씬 두 개 추가
    이거 제대로 안될 시 죽음

---
+++ SF/Assets/Scripts/FollowCam2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam2 : MonoBehaviour
+{
+    public Transform Player; // 따라다닐 타겟 오브젝트의 Transform
+    private Transform Camera; // 카메라 자신의 Transform
+
+    void Start()
+    {
+        Camera = GetComponent<Transform>();
+    }
+
+    void LateUpdate()
+    {
+        // Camera.position = new Vector3(Player.position.x, Camera.position.y, Player.position.z - 6.56f);
+        Camera.position = new Vector3(Player.position.x, Camera.position.y, Player.position.z - 9f);
+
+        Camera.LookAt(Player);
+    }
+}
---
+++ SF/Assets/Scripts/PlatformTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTrigger : MonoBehaviour
+{
+    public MovingPlatform platform;
+    public BulletSpawner spawner;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        platform.NextPlatform();
+    }
+}
