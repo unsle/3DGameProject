c220826cbfea508ac602ba77abd8f4825f2c2502
unsle <nayyeon.dgu@gmail.com>
2021-06-12 17:59:20 +0900

    Revert "change?2"
    This reverts commit c0b6ad1871571e72b6c7053a04fb84d65a98035d.

--- SF/Assets/Scripts/FollowCam.cs
+++ SF/Assets/Scripts/FollowCam.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
+<<<<<<< HEAD
     public Transform _player;        // 따라다닐 타겟 오브젝트의 Transform
     private Transform _camera;                // 카메라 자신의 Transform
 
     void Start()
     {
         _camera = GetComponent<Transform>();
+=======
+    public Transform Player; // 따라다닐 타겟 오브젝트의 Transform
+    private Transform Camera; // 카메라 자신의 Transform
+
+    void Start()
+    {
+        Camera = GetComponent<Transform>();
+>>>>>>> Seo
     }
 
     void LateUpdate()
     {
+<<<<<<< HEAD
         _camera.position = new Vector3(_player.position.x - 0.52f, _camera.position.y , _player.position.z - 6.56f);
 
         _camera.LookAt(_player);
-        _camera.position = new Vector3(_player.position.x - 0.52f, _camera.position.y , _player.position.z - 6.56f);
+=======
+        Camera.position = new Vector3(Player.position.x - 0.52f, Camera.position.y , Player.position.z - 6.56f);
 
-        _camera.LookAt(_player);
+        Camera.LookAt(Player);
+>>>>>>> Seo
     }
 }
 
